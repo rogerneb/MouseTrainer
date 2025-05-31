4861670330a5eda253018f786023290535707ec1
rogerneb <roger.neb@gmail.com>
2025-05-31 15:01:31 +0200

    minor changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function() { //when ready
     class BattlefieldClass {
-        constructor(id){
-            this.canvas = $("#"+id);
+        constructor(id) {
+            this.canvas = $("#"+id); //get canvas attributes
             this.canvasWidth = this.canvas.width();
             this.canvasHeight = this.canvas.height();
-            this.context = this.canvas.get(0).getContext("2d");//public
+            this.context = this.canvas.get(0).getContext("2d");
         }
 
-        clean(){
+        clean() { //clean screen
             this.context.clearRect(0, 0, this.canvasWidth,this.canvasHeight);
         }
 
-        print_balls = function(x,y,radi,color){
+        print_balls = function(x,y,radius,color) {
             this.context.fillStyle = color;
             this.context.beginPath();
-            this.context.arc(x, y, radi, 0, Math.PI*2);
+            this.context.arc(x, y, radius, 0, Math.PI*2);
             this.context.closePath();
             this.context.fill();
         }
     }
     
     var Battlefield = new BattlefieldClass("battlefield"); //create canvas
-    Battlefield.print_balls(10, 10, 5, "red");
+    
+    Battlefield.print_balls(250, 200, 15, "red");
+    Battlefield.print_balls(100, 105, 10, "green");
 })
